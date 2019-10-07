f29312c1755f00d5734d90704dc1b77f6adb6452
duncdrum <d.paterson@me.com>
2019-10-07 22:31:57 +0200

    test(e2e): init vscode int test scaffold

---
+++ client/src/test/extension.spec.ts
@@ -0,0 +1,8 @@
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+
+describe("vscode e2e tests", () => {
+	it("should be able to execute a test", () => {
+	  1 === 1;
+	});
+  });
--- test/xqlang.spec.ts
+++ test/xqlang.spec.ts
@@ -1,11 +1,8 @@
 import { equal } from "assert";
-// import index from "./";
+
 
 describe("Typescript usage suite", () => {
   it("should be able to execute a test", () => {
     equal(true, true);
   });
-//   it("should return expected string", () => {
-//     equal(index("incoming"), "incoming-static");
-//   });
 });
